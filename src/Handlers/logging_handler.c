411e693681d1444cc907ebc671416ed06ee1b09b
Adam Rao <adamokoth@protonmail.com>
2025-11-16 20:43:30 +0300

    chore: create logger mutex

--- src/Handlers/logging_handler.c
+++ src/Handlers/logging_handler.c
@@ -1,12 +1,17 @@
 #include <errno.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <time.h>
+
+pthread_mutex_t logger_lock = PTHREAD_MUTEX_INITIALIZER;
 
 int write_log(const char* logfile_path, const char* logfile_content)
 {
     FILE* fd = NULL;
 
     errno = 0;
+
+    pthread_mutex_lock(&logger_lock);
 
     fd = fopen(logfile_path, "a");
 
@@ -19,6 +24,8 @@ int write_log(const char* logfile_path, const char* logfile_content)
     fprintf(fd, logfile_content);
 
     fclose(fd);
+
+    pthread_mutex_unlock(&logger_lock);
 
     return 0;
 }
